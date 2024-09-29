e0cb9aad13bcb1bbdda6f12d8345ab183605a6b2
mykhailoko <ivashevbrd@gmail.com>
2024-09-29 14:03:34 +0300

    add edit feature

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -13,6 +13,9 @@ export default function TodoList({ currentView, checkStyle, setCheckStyle }) {
   const [todoValue, setTodoValue] = useState("");
   const [settingsVisible, setSettingsVisible] = useState(false);
 
+  const [editIndex, setEditIndex] = useState(null);
+  const [editTodoValue, setEditTodoValue] = useState("");
+
   useEffect(() => {
     const storedTodos = localStorage.getItem(LOCAL_STORAGE_KEY);
     setTodos(storedTodos ? JSON.parse(storedTodos) : []);
@@ -33,6 +36,21 @@ export default function TodoList({ currentView, checkStyle, setCheckStyle }) {
   const handleDeleteTodo = (index) => {
     const newTodos = todos.filter((_, i) => i !== index);
     setTodos(newTodos);
+  };
+
+  const handleEditTodo = (index) => {
+    setEditIndex(index);
+    setEditTodoValue(todos[index].text);
+  };
+
+  const handleSaveTodo = (index) => {
+    if (editTodoValue.trim()) {
+      const updatedTodos = todos.map((todo, i) =>
+        i === index ? { ...todo, text: editTodoValue } : todo
+      );
+      setTodos(updatedTodos);
+      setEditIndex(null);
+    }
   };
 
   const toggleCheck = (index) => {
@@ -83,11 +101,32 @@ export default function TodoList({ currentView, checkStyle, setCheckStyle }) {
                 />
               </button>
 
-              <p style={{ textDecoration: todo.checked ? 'line-through' : 'none' }}>
-                {todo.text}
-              </p>
+              {editIndex === index ? (
+                <input
+                  className="edit-input"
+                  value={editTodoValue}
+                  onChange={(e) => setEditTodoValue(e.target.value)}
+                  onBlur={() => handleSaveTodo(index)}
+                  onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                      handleSaveTodo(index);
+                    }
+                  }}
+                  autoFocus
+                />
+              ) : (
+                <p
+                  style={{ textDecoration: todo.checked ? 'line-through' : 'none' }}
+                  onClick={() => handleEditTodo(index)}
+                >
+                  {todo.text}
+                </p>
+              )}
               
               <div className="actionsContainer">
+                <button onClick={() => handleEditTodo(index)}>
+                  <i class="fa-solid fa-pencil"></i>
+                </button>
                 <button onClick={() => handleDeleteTodo(index)}>
                   <i className="fa-regular fa-trash-can"></i>
                 </button>
